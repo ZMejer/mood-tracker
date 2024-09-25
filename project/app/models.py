be0d13780877b9a7dbcb385f007fccd7295c9c2e
ovezthaking <oliwerx12@gmail.com>
2024-09-25 14:40:34 +0200

    test surname

--- project/app/models.py
+++ project/app/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Name(models.Model):
     name = models.CharField(max_length=30)
-
+    surname = models.CharField(max_length=35, default='')
     def __str__(self):
         return self.name
