728ae649b8e1b120b3a464f0e24ed8257bdae303
ZMejer <zosiacastiel@gmail.com>
2024-09-28 18:32:34 +0200

    poprawka

--- project/app/views.py
+++ project/app/views.py
@@ -6,8 +6,7 @@ from django.utils import timezone
 
  
 def home(request):
-    
-    return render(request, "home.html", context)
+    return render(request, "home.html")
 
 
 def register(request):
