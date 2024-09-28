8eb91c8d661586aff869ba9c53a84e6b96dc6c41
ZMejer <zosiacastiel@gmail.com>
2024-09-29 01:02:06 +0200

    rejestracja logowanie mega duzo rzeczy

--- project/app/models.py
+++ project/app/models.py
@@ -1,23 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class Name(models.Model):
-    name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=35, default='')
-    age = models.IntegerField(default=0)
-    email = models.EmailField(default='')
-    password = models.CharField(max_length=30, default='')
-    password2 = models.CharField(max_length=30, default='')
-    phone = models.CharField(max_length=15, default='')
-    address = models.CharField(max_length=50, default='')
-    city = models.CharField(max_length=30, default='')
-    postal_code = models.CharField(max_length=6, default='')
-    country = models.CharField(max_length=30, default='')
-
+class Name(AbstractUser):
+    name=models.CharField(default='', max_length=30)
+    surname=models.CharField(default='', max_length=30)
+    age=models.IntegerField(default=0)
+    calorie_goal = models.IntegerField(default=2000)
+    email=models.CharField(default='', max_length=30)
+    phone=models.IntegerField(default=0)
+    address=models.CharField(default='', max_length=30)
+    city=models.CharField(default='', max_length=30)
+    postal_code=models.CharField(default='', max_length=30)
+    country=models.CharField(default='', max_length=30)
     def __str__(self):
         return self.name
-
-
-#moje zadanie bedzie takie
-#zrobic system logowania
-#class Name to uzytkownik
-#zrobic strone z resjestracją!! i logowaniem
