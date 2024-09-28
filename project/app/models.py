fd78c031f47b170c1f2f507390f8a4aa7b425c0d
ovezthaking <oliwerx12@gmail.com>
2024-09-28 14:56:18 +0200

    Name class changed to User class

--- project/app/models.py
+++ project/app/models.py
@@ -1,7 +1,21 @@
 from django.db import models
 
-class Name(models.Model):
+class User(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=35, default='')
+    age = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    phone = models.CharField(max_length=15, default='')
+    address = models.CharField(max_length=50, default='')
+    city = models.CharField(max_length=30, default='')
+    postal_code = models.CharField(max_length=6, default='')
+    country = models.CharField(max_length=30, default='')
+
     def __str__(self):
         return self.name
+
+
+#moje zadanie bedzie takie
+#zrobic system logowania
+#class Name to uzytkownik
+#zrobic strone z resjestracją!! i logowaniem
